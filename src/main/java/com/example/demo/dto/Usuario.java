88e46eeb118a74069fcbed44e5671e0d5dbbc5a5
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-26 12:16:49 +0200

    Se aplican ficheros security y authenticators

---
+++ src/main/java/com/example/demo/dto/Usuario.java
@@ -0,0 +1,75 @@
+package com.example.demo.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private long id;
+	@Column(name="username")
+	private String username;
+	@Column(name="password")
+	private String password;
+	@Column(name="role")
+	private String role;
+	
+	
+	// Creamos constructores
+	public Usuario() {
+		
+	}
+	
+	public Usuario(long id, String username, String password, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	// Getters y setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	// Creamos el toString
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
+	}	
+}
